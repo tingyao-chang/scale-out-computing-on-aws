0f516fa45cde123c3fe80d7026ff38ca68cf61d4
tingyao-chang <karaokstar@gmail.com>
2021-02-09 09:07:02 +0000

    Change pbs stop script, and delete duplicated Chrony setting due to CloudFormation already did.

--- source/soca/cluster_node_bootstrap/ComputeNode.sh
+++ source/soca/cluster_node_bootstrap/ComputeNode.sh
@@ -9,7 +9,7 @@ if [ $# -lt 1 ]
 fi
 
 # In case AMI already have PBS installed, force it to stop
-service pbs stop
+systemctl stop pbs
 
 # Install SSM
 yum install -y https://s3.$AWS_DEFAULT_REGION.amazonaws.com/amazon-ssm-$AWS_DEFAULT_REGION/latest/linux_amd64/amazon-ssm-agent.rpm
@@ -268,44 +268,11 @@ EOF
     sudo grub2-mkconfig -o /boot/grub2/grub.cfg
 fi
 
-# Configure Chrony
-yum remove -y ntp
-mv /etc/chrony.conf  /etc/chrony.conf.original
-echo -e """
-# use the local instance NTP service, if available
-server 169.254.169.123 prefer iburst minpoll 4 maxpoll 4
-
-# Use public servers from the pool.ntp.org project.
-# Please consider joining the pool (http://www.pool.ntp.org/join.html).
-# !!! [BEGIN] SOCA REQUIREMENT
-# You will need to open UDP egress traffic on your security group if you want to enable public pool
-#pool 2.amazon.pool.ntp.org iburst
-# !!! [END] SOCA REQUIREMENT
-# Record the rate at which the system clock gains/losses time.
-driftfile /var/lib/chrony/drift
-
-# Allow the system clock to be stepped in the first three updates
-# if its offset is larger than 1 second.
-makestep 1.0 3
-
-# Specify file containing keys for NTP authentication.
-keyfile /etc/chrony.keys
-
-# Specify directory for log files.
-logdir /var/log/chrony
-
-# save data between restarts for fast re-load
-dumponexit
-dumpdir /var/run/chrony
-""" > /etc/chrony.conf
-systemctl enable chronyd
-
 # Disable ulimit
 echo -e  "
 * hard memlock unlimited
 * soft memlock unlimited
 " >> /etc/security/limits.conf
-
 
 # Reboot to disable SELINUX
 sudo reboot
