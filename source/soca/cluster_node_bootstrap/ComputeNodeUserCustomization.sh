585b6df56cc24c7c3704f7adaab4f320f4d0a4ad
tingyao-chang <karaokstar@gmail.com>
2021-03-05 03:58:15 +0000

    Add audit rules to track all commands run by root

--- source/soca/cluster_node_bootstrap/ComputeNodeUserCustomization.sh
+++ source/soca/cluster_node_bootstrap/ComputeNodeUserCustomization.sh
@@ -2,4 +2,11 @@
 
 # User customization code below
 echo -e "umask 027" > /etc/profile.d/umask.sh
+
 timedatectl set-timezone "Asia/Taipei"
+
+echo "
+## Track all commands run by root
+-a exit,always -F arch=b64 -F euid=0 -S execve
+-a exit,always -F arch=b32 -F euid=0 -S execve
+" >> /etc/audit/rules.d/audit.rules
